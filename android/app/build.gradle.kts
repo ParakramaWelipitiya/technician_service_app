2aff70aa6985b21000274df1f7b21edadd465955
Parakrama Welipitiya <parakramawelipitiya00@gmail.com>
2026-04-11 04:51:31 +0530

    1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
     // ndkVersion = "27.0.12077973"
+    
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
